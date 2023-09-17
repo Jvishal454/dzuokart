d896d0c246d4f0aab8d24e6ff4f002d7fd395cde
Vishal Jaiswal <jvishal8794@gmail.com>
2023-09-17 23:33:48 +0530

    assets & grids

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  src: string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+
+  tiles: Tile[] = [
+    {src: 'assets/images/home-carousel/grid-1.jpg', cols: 3, rows: 1, color: 'lightgreen'},
+    {src: 'assets/images/home-carousel/grid-1.jpg', cols: 1, rows: 2, color: 'dodgerblue'},
+    {src: 'assets/images/home-carousel/grid-1.jpg', cols: 1, rows: 1, color: '#EAC562'},
+    {src: 'assets/images/home-carousel/grid-1.jpg', cols: 2, rows: 1, color: '#EF1C74'},
+  ];
 
 }
